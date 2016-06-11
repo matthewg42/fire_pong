b16682900d19ffa23373d71ac78bd7171878dbb5
Matthew Gates <matthewg42@gmail.com>
2016-06-11 23:56:40 +0100

    remove debugs

--- Seq.cpp
+++ Seq.cpp
@@ -6,28 +6,23 @@ Seq::Seq()
 	for (int i=0; i<MAX_SEQ_SIZE; i++) {
 		_events[i] = NULL;
 	}
-	Serial.println(F("Seq::Seq()"));
 }
 
 Seq::~Seq()
 {
-	Serial.println(F("Seq::~Seq GNDN"));
 }
 
 void Seq::append(Evt *e) {
 	for (int i=0; i<MAX_SEQ_SIZE; i++) {
 		if (_events[i] == NULL) {
-			Serial.println(F("Seq::appended"));
 			_events[i] = e;
 			return;
 		}
 	}
-	Serial.println(F("Seq::append no room"));
 }
 
 void Seq::start(int activations)
 {
-	Serial.println(F("Seq::Seq()"));
 	for (int i=0; i<MAX_SEQ_SIZE; i++) {
 		if (_events[i] != NULL) {
 			_events[i]->start(activations);
