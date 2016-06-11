017be5928afe9f08945c50ca654178e6acb7ae4d
Matthew Gates <matthewg42@gmail.com>
2016-06-12 00:33:10 +0100

    example with overlapping sequences

--- main.ino
+++ main.ino
@@ -1,30 +1,68 @@
+#include <Arduino.h>
 #include "Evt.h"
 #include "Seq.h"
 
-void switch_relay(int id, bool on) {
-    Serial.print(F("Switching relay #"));
-    Serial.print(id);
-    Serial.println(on ? F(" on") : F(" off"));
+#define RELAY_ON        LOW
+#define RELAY_OFF       HIGH
+
+Seq pufferSeq;
+Seq relaySeq[4];
+
+unsigned char PUFFER_PINS[4][2] = {  
+   {2, 3} , // puffer 0
+   {4, 5} , // puffer 1
+   {6, 7} , // puffer 2
+   {8, 9}   // puffer 3
+};
+
+void activate_puffer(int id, bool on) {
+    Serial.print(F("PUFF:"));
+    Serial.println(id);
+    relaySeq[id].start(1);   
 }
 
-Seq puff1;
+void set_pin(int pin, bool state) {
+    Serial.print(F("Relay PIN "));
+    Serial.print(pin);
+    Serial.print(F(" : "));
+    Serial.println(state==RELAY_ON ? F("ON") : F("OFF"));
+    digitalWrite(pin, state);
+}
 
 void setup()
 {
     Serial.begin(115200);
-    puff1.append(new EvtCallbackIntBool(0,    0, switch_relay, 0, true));
-    puff1.append(new EvtCallbackIntBool(100,  0, switch_relay, 1, true));
-    puff1.append(new EvtCallbackIntBool(113,  0, switch_relay, 1, false));
-    puff1.append(new EvtCallbackIntBool(313,  0, switch_relay, 1, true));
-    puff1.append(new EvtCallbackIntBool(463,  0, switch_relay, 1, false));
-    puff1.append(new EvtCallbackIntBool(763,  0, switch_relay, 0, false));
-    puff1.start(1);
-    Serial.println(F("setup end"));
+
+    // Set relay pins for output
+    for(unsigned char i=0; i<4; i++) {
+        pinMode(PUFFER_PINS[i][0], OUTPUT);     
+        set_pin(PUFFER_PINS[i][0], RELAY_OFF);
+        pinMode(PUFFER_PINS[i][1], OUTPUT);     
+        set_pin(PUFFER_PINS[i][1], RELAY_OFF);
+        relaySeq[i].append(new EvtCallbackIntBool(0,    0, set_pin, PUFFER_PINS[i][0], RELAY_ON));
+        relaySeq[i].append(new EvtCallbackIntBool(100,  0, set_pin, PUFFER_PINS[i][1], RELAY_ON));
+        relaySeq[i].append(new EvtCallbackIntBool(113,  0, set_pin, PUFFER_PINS[i][1], RELAY_OFF));
+        relaySeq[i].append(new EvtCallbackIntBool(313,  0, set_pin, PUFFER_PINS[i][1], RELAY_ON));
+        relaySeq[i].append(new EvtCallbackIntBool(463,  0, set_pin, PUFFER_PINS[i][1], RELAY_OFF));
+        relaySeq[i].append(new EvtCallbackIntBool(763,  0, set_pin, PUFFER_PINS[i][0], RELAY_OFF));
+    }
+
+    // Add four puff enents - one for each puffer
+    pufferSeq.append(new EvtCallbackIntBool(1000, 1, activate_puffer, 0, true));
+    pufferSeq.append(new EvtCallbackIntBool(1500, 1, activate_puffer, 1, true));
+    pufferSeq.append(new EvtCallbackIntBool(2000, 1, activate_puffer, 2, true));
+    pufferSeq.append(new EvtCallbackIntBool(2500, 1, activate_puffer, 3, true));
+
+    Serial.println(F("setup() done"));
 }
 
 void loop()
 {
-    puff1.tick();
+    // switch puffer relays as appropriate
+    pufferSeq.tick();
+    for(unsigned char i=0; i<4; i++) {
+        relaySeq[i].tick();
+    }
 }
 
 
