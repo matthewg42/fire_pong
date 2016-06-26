331880a136a7f126bf37e98c6672d798bdd31e80
Matthew Gates <matthewg42@gmail.com>
2016-06-26 04:07:46 +0100

    multiple relay sequences

--- src/event/relay_pong/main.ino
+++ src/event/relay_pong/main.ino
@@ -5,26 +5,35 @@
 
 void handle_event(fp_event& e);
 
-RelayReceiver relay(0x01, 2);
+RelayReceiver *relay[8];
 EventBuffer buf(handle_event);
 
 void setup() {
 	Serial.begin(115200);
     delay(500);
-    relay.setup();
+    uint32_t id = 0x1;
+    for (int i=0; i<8; i++) {
+        relay[i] = new RelayReceiver(id, 2+i);
+        relay[i]->setup();
+        id = id << 1;
+    }
 	Serial.println(F("setup complete"));
 }
 
 void loop () {
     buf.tick();
-    relay.tick();
+    for (int i=0; i<8; i++) {
+        relay[i]->tick();
+    }
 }
 
 void handle_event(fp_event& e)
 {
     Serial.print("RECV: ");
     e.dump();
-    relay.handle(e);
+    for (int i=0; i<8; i++) {
+        relay[i]->handle(e);
+    }
 }
 
 
