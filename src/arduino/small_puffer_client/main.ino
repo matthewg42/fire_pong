eb94c5b4480b964ba5cf7fb95aac3266e6f2ebff
Matthew Gates <matthewg42@gmail.com>
2016-07-07 22:48:59 +0100

    add heartbeat

--- src/arduino/small_puffer_client/main.ino
+++ src/arduino/small_puffer_client/main.ino
@@ -4,14 +4,24 @@
 #include <SmallPufferReceiver.h>
 #include <RelayReceiver.h>
 
+#define HEARTBEAT_PIN    13
+#define HEARTBEAT_OFF_MS 500
+#define HEARTBEAT_ON_MS  50
+
 void handle_event(FpEvent& e);
+void heartbeat_tick();
 
 SmallPufferReceiver *puffer[4];
 EventBuffer buf(handle_event);
+unsigned long lastHeartbeat = 0;
+bool heartbeatOn = true;
 
 void setup() {
     uint32_t id = 0x1;
     Serial.begin(115200);
+
+    pinMode(HEARTBEAT_PIN, OUTPUT);
+    digitalWrite(HEARTBEAT_PIN, heartbeatOn);
 
     // Should define PUFFER_SEGMENT_NUMBER in CFLAGS
     for (uint8_t i=1; i<PUFFER_SEGMENT_NUMBER; i++) {
@@ -33,6 +43,17 @@ void loop () {
     buf.tick();
     for (uint8_t i=0; i<4; i++) {
         puffer[i]->tick();
+    }
+    heartbeat_tick();
+}
+
+void heartbeat_tick()
+{
+    unsigned long now = millis();
+    if (now - lastHeartbeat > (heartbeatOn ? HEARTBEAT_ON_MS : HEARTBEAT_OFF_MS)) {
+        heartbeatOn = !heartbeatOn;
+        digitalWrite(HEARTBEAT_PIN, heartbeatOn);
+        lastHeartbeat = now;
     }
 }
 
