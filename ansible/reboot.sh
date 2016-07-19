25ed4e7dd867a18727530ebac612e4d7f9141e83
Matthew Gates <matthewg42@gmail.com>
2016-07-19 11:03:43 +0100

    remove pi user and use root

--- ansible/reboot.sh
+++ ansible/reboot.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+if [ $# -eq 2 ]; then
+    user="$1"
+    host="$2"
+    newuser="$1"
+    newhost="$2"
+elif [ $# -eq 4 ]; then
+    user="$1"
+    host="$2"
+    newuser="$3"
+    newhost="$4"
+else
     cat <<EOD
 Usage:
-  $0 username hostname [newhostname]
+  $0 username hostname [newuser newhost]
 
 EOD
     exit 1
 fi
 
-user="$1"
-shift
-reboothost="$1"
-shift
-waithost="$1"
-[ "$waithost" = "" ] && waithost="$reboothost"
-
-ssh "$user@$reboothost" sudo reboot
+if [ "$user" != "root" ]; then
+    ssh "$user@$host" sudo reboot
+else
+    ssh "$user@$host" reboot
+fi
 sleep 5
+
 n=0
-cmd="ping -c 1 -W 1 $waithost"
+cmd="ping -c 1 -W 1 $newhost"
 while ! $cmd > /dev/null 2>&1; do
     if [ $n -eq 0 ]; then
-        echo -n "waiting for $waithost to be pingable "
+        echo -n "waiting for $newhost to be pingable "
     else
         echo -n "."
     fi  
     sleep 1 
     let n+=1
 done
-echo "$waithost pingable..."
+echo "$newhost pingable..."
 
 n=0
-cmd="ssh $user@$waithost exit"
+cmd="ssh $newuser@$newhost exit"
 while ! $cmd > /dev/null 2>&1; do
     if [ $n -eq 0 ]; then
-        echo -n "waiting for ssh to come up for $user@$waithost "
+        echo -n "waiting for ssh to come up for $newuser@$newhost "
     else
         echo -n "."
     fi  
