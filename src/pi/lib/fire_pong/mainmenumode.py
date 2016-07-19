4e4bbf488266f3a80d02cc83e1eac039999ea6ae
Matthew Gates <matthewg42@gmail.com>
2016-07-19 14:21:09 +0100

    add setup menu and sparker test mode

--- src/pi/lib/fire_pong/mainmenumode.py
+++ src/pi/lib/fire_pong/mainmenumode.py
@@ -1,11 +1,12 @@
 from fire_pong.menumode import MenuMode
 from fire_pong.pongmode import PongMode, PongVictory
 from fire_pong.continuousmode import ContinuousMode
+from fire_pong.setupmode import SetupMenuMode
 from fire_pong.singlemode import SingleMode
 
 class MainMenuMode(MenuMode):
     ''' Main Menu '''
     def __init__(self):
-        MenuMode.__init__(self, [PongMode, ContinuousMode, SingleMode, PongVictory])
+        MenuMode.__init__(self, [PongMode, SetupMenuMode, ContinuousMode, SingleMode, PongVictory])
 
 
---
+++ src/pi/lib/fire_pong/setupmode.py
@@ -0,0 +1,75 @@
+import time
+import threading
+import struct
+from fire_pong.util import log, config
+from fire_pong.mode import Mode
+from fire_pong.scoreboard import ScoreBoard
+from fire_pong.events import *
+from fire_pong.modemanager import ModeManager
+from fire_pong.fp_event import FpEvent
+from fire_pong.fp_serial import FpSerial
+from fire_pong.menumode import MenuMode
+
+class SetupMenuMode(MenuMode):
+    __displayname__ = 'SU'
+    def __init__(self):
+        MenuMode.__init__(self, [TestSparkers])
+
+class TestSparkers(Mode):
+    __displayname__ = 'SP'
+    RELAY_ON = struct.pack('<H', 0)
+    RELAY_OFF = struct.pack('<H', 1)
+    ''' Spark a single spearker, selected with swipes '''
+    def __init__(self):
+        log.debug('TestSparkers.__init__() START')
+        Mode.__init__(self)
+        self.puffers = config['PongGame']['puffers']
+        self.duration = 1  # in seconds
+        self.idx = 0
+        self.display = True
+        self.activate = False
+
+    def run(self):
+        log.debug('TestSparkers.run() START')
+        while not self.terminate:
+            if self.activate:
+                self.activate = False
+                self.start_spark()
+                time.sleep(self.duration)
+                self.stop_spark()
+            if self.display:
+                log.info('TestSparkers select puffer idx=%d; id=0x%08X; press START to activate' % (self.idx, self.puffers[self.idx]))
+                ScoreBoard().display('%02d' % self.idx)
+                self.display = False
+            time.sleep(0.2)
+        log.debug('TestSparkers.run() END')
+        return None
+
+    def start_spark(self):
+        log.info("SPARK START idx=%02d id=%08X" % (self.idx, self.puffers[self.idx]))
+        e = FpEvent(self.puffers[self.idx], 'FP_EVENT_SPARK', self.RELAY_ON)
+        log.info(str(e))
+        FpSerial().write(e.serialize())
+
+    def stop_spark(self):
+        log.info("SPARK STOP idx=%02d id=%08X" % (self.idx, self.puffers[self.idx]))
+        e = FpEvent(self.puffers[self.idx], 'FP_EVENT_SPARK', self.RELAY_OFF)
+        log.info(str(e))
+        FpSerial().write(e.serialize())
+        
+    def event(self, event):
+        log.debug('TestSparkers.event: received %s' % str(event))
+        if event == EventQuit():
+            self.terminate = True
+
+        if event == EventButton('start'):
+            # set the flag to do the actual work in run()
+            self.activate = True
+
+        if type(event) is EventSwipe:
+            if event.player == '2UP': 
+                self.idx = (self.idx + 1) % len(self.puffers)
+            else:
+                self.idx = (self.idx - 1) % len(self.puffers)
+            self.display = True
+
