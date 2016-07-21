40766776044b5a17dde0aeb8e3f37ff610a66e4a
Matthew Gates <matthewg42@gmail.com>
2016-07-21 11:19:05 +0100

    integrate MusicMode

--- src/pi/lib/fire_pong/mainmenumode.py
+++ src/pi/lib/fire_pong/mainmenumode.py
@@ -2,10 +2,11 @@ from fire_pong.menumode import MenuMode
 from fire_pong.pongmode import PongMode, PongVictory
 from fire_pong.continuousmode import ContinuousMode
 from fire_pong.setupmode import SetupMenuMode
+from fire_pong.musicmode import MusicMode
 
 class MainMenuMode(MenuMode):
     ''' Main Menu '''
     def __init__(self):
-        MenuMode.__init__(self, [PongMode, SetupMenuMode, ContinuousMode, PongVictory])
+        MenuMode.__init__(self, [PongMode, SetupMenuMode, ContinuousMode, PongVictory, MusicMode])
 
 
