8dba0e59926e52e0969f5747cd4c4a0faf72f390
Matthew Gates <matthewg42@gmail.com>
2016-07-21 13:07:34 +0100

    quick and easy manual mode

--- src/pi/lib/fire_pong/mainmenumode.py
+++ src/pi/lib/fire_pong/mainmenumode.py
@@ -3,10 +3,11 @@ from fire_pong.pongmode import PongMode, PongVictory
 from fire_pong.continuousmode import ContinuousMode
 from fire_pong.setupmode import SetupMenuMode
 from fire_pong.musicmode import MusicMode
+from fire_pong.manualmode import ManualMode
 
 class MainMenuMode(MenuMode):
     ''' Main Menu '''
     def __init__(self):
-        MenuMode.__init__(self, [PongMode, SetupMenuMode, ContinuousMode, PongVictory, MusicMode])
+        MenuMode.__init__(self, [PongMode, SetupMenuMode, ContinuousMode, PongVictory, MusicMode, ManualMode])
 
 
---
+++ src/pi/lib/fire_pong/manualmode.py
@@ -0,0 +1,54 @@
+import time
+import threading
+import random
+from os import listdir
+from struct import pack, unpack
+from fire_pong.util import log, config
+from fire_pong.mode import Mode
+from fire_pong.events import *
+from fire_pong.scoreboard import ScoreBoard
+from fire_pong.modemanager import ModeManager
+from fire_pong.fp_event import FpEvent
+from fire_pong.fp_serial import FpSerial
+
+
+class ManualMode(Mode):
+    __displayname__ = 'MA'
+    ''' Make puffs happen with buttons
+        START button: random small puffer
+        SWIPE1 large puff 1
+        SWIPE2 large puff 2 '''
+    def __init__(self):
+        Mode.__init__(self)
+        log.debug('ManualMode.__init__()')
+        self.small_puffers = config['PongGame']['puffers']
+        self.large_puffers = config['LargePuffers']['ids']
+        self.puff_duration = 50
+        self.puffer_mask = 0
+
+    def run(self):
+        log.debug('ManualMode.run() START')
+        ScoreBoard().display(ManualMode.__displayname__.lower())
+        while not self.terminate:
+            if self.puffer_mask != 0:
+                e = FpEvent(self.puffer_mask, 'FP_EVENT_PUFF', pack('<H', self.puff_duration))
+                log.info('PUFF event: %s' % str(e))
+                FpSerial().write(e.serialize())
+                self.puffer_mask = 0
+
+        log.debug('ManualMode.run() END')
+
+    def event(self, event):
+        log.debug('ManualMode.event(%s)' % str(event))
+        if event == EventQuit():
+            self.terminate = True
+
+        if event == EventButton('start'):
+            self.puffer_mask = self.puffer_mask | self.small_puffers[random.randint(0, len(self.small_puffers)-1)]
+
+        if type(event) is EventSwipe:
+            if event.player == '2UP': 
+                self.puffer_mask = self.puffer_mask | self.large_puffers[0]
+            else:
+                self.puffer_mask = self.puffer_mask | self.large_puffers[-1]
+
