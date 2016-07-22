477fb8037b0be957841dad17aaccde9364b3bbac
Matthew Gates <matthewg42@gmail.com>
2016-07-22 22:05:42 +0100

    FIXES: 5a (Watchdog)

--- src/arduino/small_puffer_client/main.ino
+++ src/arduino/small_puffer_client/main.ino
@@ -4,6 +4,7 @@
 #include <SmallPufferReceiver.h>
 #include <RelayReceiver.h>
 #include <Heartbeat.h>
+#include <avr/wdt.h>
 
 #define HEARTBEAT_PIN    13
 
@@ -17,25 +18,39 @@ void setup() {
     uint32_t id = 0x1;
     Serial.begin(115200);
 
+    // Set up the heartbeat and set it to flash rapidly during setup()
     heartbeat.setup();
+    heartbeat.setMode(Heartbeat::Quick);
 
     // Should define PUFFER_SEGMENT_NUMBER in CFLAGS
     for (uint8_t i=1; i<PUFFER_SEGMENT_NUMBER; i++) {
         id = id << 4;
     }
 
+    // Determine IDs for this segment
     for (uint8_t i=0; i<4; i++) {
         puffer[i] = new SmallPufferReceiver(id, (i*2)+3, (i*2)+2);
         puffer[i]->setup();
         id = id << 1;
     }
 
-    delay(500);
+    // Let the serial line settle and let observers see rapid heartbeat indicating reset
+    int wait = millis() + 400;
+    while(millis() < wait) {
+        heartbeat.tick();
+    }
+
 #ifdef DEBUG
 	Serial.print(F("small puffer firmware segment "));
 	Serial.print(PUFFER_SEGMENT_NUMBER);
     Serial.println(F(" setup complete"));
 #endif
+
+    // Enable watchdog timer (250 ms before reset)
+    wdt_enable(WDTO_250MS);
+
+    // Indicate ent of setup
+    heartbeat.setMode(Heartbeat::Normal);
 }
 
 void loop () {
@@ -44,6 +59,7 @@ void loop () {
     for (uint8_t i=0; i<4; i++) {
         puffer[i]->tick();
     }
+    wdt_reset();
 }
 
 void handle_event(FpEvent& e)
